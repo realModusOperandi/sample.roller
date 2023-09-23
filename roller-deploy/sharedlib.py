87633538c396bea48291fc2085757a48401cfe9e
realModusOperandi <liamwestby@icloud.com>
2023-09-23 16:39:38 -0500

    Start tWAS deployment scripts

---
+++ roller-deploy/sharedlib.py
@@ -0,0 +1,9 @@
+# Create shared library
+AdminConfig.create('Library', AdminConfig.getid('/Cell:DefaultCell01/Node:DefaultNode01/Server:server1/'), '[[nativePath ""] [name "roller-lib"] [isolatedClassLoader false] [description ""] [classPath "/opt/IBM/WebSphere/libs/roller-lib.jar"]]')
+AdminConfig.save()
+
+# Create shared library reference
+AdminConfig.create('Classloader', '(cells/DefaultCell01/nodes/DefaultNode01/servers/server1|server.xml#ApplicationServer_1183122130078)', '[[mode PARENT_FIRST]]')
+classLoad1 = AdminConfig.list('Classloader', AdminConfig.getid( '/Cell:DefaultCell01/Node:DefaultNode01/Server:server1/'))
+AdminConfig.create('LibraryRef', classLoad1, [['libraryName', 'roller-lib']])
+AdminConfig.save()
